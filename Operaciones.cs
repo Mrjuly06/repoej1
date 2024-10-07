d37ceaf9825fd40ef9be02df138b433fc9392ef7
Mrjuly06 <jcvalladares@uamv.edu.ni>
2024-10-07 08:37:49 -0600

    Agregar archivos de proyecto.

---
+++ Operaciones.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejercicio1calculadora
+{
+    internal class Operaciones
+    {
+        public double add( double num1, double num2) {  return num1 + num2; }
+        public double Subtracion(double num1, double num2) {  return num1 - num2; }
+        public double Multiplication(double num1 , double num2) { return num1 * num2; }
+        public double diviction (double num1, double num2) { 
+        try
+            {
+                return num1 / num2;
+            }
+            catch (Exception ex) 
+            { Console.WriteLine(ex.message); }
+            return 0;
+        }
+        public double power(double num1, double num2) { return Math.Pow(num1 * num2); }
+        public double squarer(double num) {  return Math.Sqrt(num); }
+        public string shutdown()
+        {
+            return "Adios";
+        }
+    }
+}
